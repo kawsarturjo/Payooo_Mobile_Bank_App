56684286d6334d2ad0d70b57d8e0311a3ee6c5be
KawsarMohammadTurjo <kawsarmohammadturjo@gmail.com>
2026-07-04 00:43:40 +0600

    login page complete

---
+++ Script/login.js
@@ -0,0 +1,24 @@
+      document.getElementById('loginBtn').addEventListener('click', function(){
+
+        // get the mobile number
+        const mobileNumberArea = document.getElementById('mobileNumber').value;
+        mobileNumber.value = '';
+
+        // get the pin
+        const pinArea = document.getElementById('pin').value;
+        pin.value = '';
+        
+        // Match the mobile number and pin with the stored values
+        if(mobileNumberArea === '01715257515' && pinArea === '7788'){
+
+          // Redirect to dashboard.html
+          alert("Login Successful!!")
+          window.location.assign("/homepage.html")
+        } else {
+
+          // Show an alert for invalid credentials
+          alert('Invalid mobile number or pin. Please try again.');
+          return;
+        }
+        
+      });
